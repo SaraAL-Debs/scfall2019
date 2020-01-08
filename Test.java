c9162a3e54ea3ba392b3033e4996274c7670f29a
SaraAL-Debs <Sara.Debs.99@gmail.com>
2020-01-08 20:16:30 +0200

    new obj is added

--- Test.java
+++ Test.java
@@ -21,7 +21,11 @@ public class Test
         System.out.println (p.getName ());		
 
         Person p= new Person ("Lobo", 33);
-        System.out.println (p.getName ());		
+        System.out.println (p.getName ());
+
+        Person p= new Person ("SaraAL-Debs", 44);
+        System.out.println (p.getName ());
+		
 
     }
 }
